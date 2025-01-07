3cb9598b7cae24f6eda471daa411c710e5d2d502
FateTech <fatetech007@gmail.com>
2025-01-07 14:36:45 +0100

    setting up my nav-bar skeleton

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,31 @@
 import react from 'react'
 import './Navbar.css'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 
 const Navbar = () => {
 
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <div className="header">
+            {/* Logo */}
+            <div className="container">
+                <h1>De<span className="primary">Fi</span></h1>
+                <ul className="nav-menue">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Feature</a></li>
+                    <li><a href="/">Earn</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+                <div className="btn-group">
+                    <button className="btn">connect walllet</button>
+
+                </div>
+                <div className="hambuger">
+                    <FaBars />
+                </div>
+
+            </div>
+        </div>
     )
 }
 
