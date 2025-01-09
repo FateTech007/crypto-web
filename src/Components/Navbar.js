de401b9a1d0e62df313a9e863c0ec4a4aaefcecc
FateTech <fatetech007@gmail.com>
2025-01-09 21:41:13 +0100

    complete nav-menue styling and functionality

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,24 @@
-import react from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 
 const Navbar = () => {
 
+    const [showNavMenue, setShowNavMenue] = useState(false)
+
+
+    const handleNavMenue = () => {
+        setShowNavMenue(!showNavMenue)
+
+    }
+
     return (
         <div className="header">
             {/* Logo */}
             <div className="container">
                 <h1>De<span className="primary">Fi</span></h1>
-                <ul className="nav-menue">
+                <ul className={showNavMenue ? "nav-menue active" : "nav-menue"}>
                     <li><a href="/">Home</a></li>
                     <li><a href="/">Feature</a></li>
                     <li><a href="/">Earn</a></li>
@@ -20,8 +28,11 @@ const Navbar = () => {
                     <button className="btn">connect walllet</button>
 
                 </div>
-                <div className="hambuger">
-                    <FaBars size={20} style={{ color: "#118c4f" }} />
+                <div className="hambuger" onClick={handleNavMenue}>
+                    {
+                        showNavMenue ? <FaTimes size={20} style={{ color: "#118c4f" }} /> : <FaBars size={20} style={{ color: "#118c4f" }} />
+                    }
+
                 </div>
 
             </div>
